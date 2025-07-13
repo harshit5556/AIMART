000b5268075956240404731dd6f0c7aca88abd06
Harshit Tiwari <122154874+harshit5556@users.noreply.github.com>
2025-07-13 17:34:17 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
 app.use(express.json())
 app.use(cookieParser()) 
 app.use(cors({
-    origin:["http://localhost:5173","http://localhost:5174"],
+    origin:["https://aimart-frontend.onrender.com","http://localhost:5174"],
     credentials:true
 
 }))
